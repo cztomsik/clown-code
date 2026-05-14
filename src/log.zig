3b9bc4c2536d6a0e1c5ada6ff5bed8bfcdb4f36f
Kamil Tomsik <info@tomsik.cz>
2026-05-14 09:27:25 +0200

    log: fix append-writing

--- src/log.zig
+++ src/log.zig
@@ -1,38 +1,28 @@
 const std = @import("std");
 const tk = @import("tokamak");
 
-// Custom std.log implementation that writes everything to debug.log.
-// Opens the file once and appends, replacing the default stderr logging.
-var log_file: ?std.fs.File = null;
-var log_mutex = std.Thread.Mutex{};
+const FILE = "debug.log";
 
+// Custom std.log implementation that append-writes everything to a file.
 pub fn debugLog(
     comptime level: std.log.Level,
     comptime scope: anytype,
     comptime format: []const u8,
     args: anytype,
 ) void {
-    log_mutex.lock();
-    defer log_mutex.unlock();
+    // NOTE: This could still race but I don't want to waste any more time with legacy I/O
+    const file = std.fs.cwd().openFile(FILE, .{ .mode = .read_write, .lock = .exclusive }) catch std.fs.cwd().createFile(FILE, .{}) catch return;
+    defer file.close();
 
-    if (log_file == null) {
-        const file = std.fs.cwd().createFile("debug.log", .{ .truncate = false }) catch return;
-        if (file.stat()) |s| file.seekTo(s.size) catch {} else |_| {}
-        log_file = file;
-    }
+    file.seekFromEnd(0) catch {};
 
-    const file = log_file.?;
     var buf: [4096]u8 = undefined;
-    var fbs = std.io.fixedBufferStream(&buf);
-    var w = fbs.writer();
+    var bw = file.writerStreaming(&buf); // NOTE: writer() ignores seeking entirely, and it took me a while to figure it out!
+    var w = &bw.interface;
+    defer w.flush() catch {};
 
-    const timestamp = tk.time.Time.now();
-    timestamp.format(w) catch return;
-    w.print(" {s} {s} ", .{ @tagName(level), @tagName(scope) }) catch return;
+    w.print("{f} {s} {s} ", .{ tk.time.Time.now(), @tagName(level), @tagName(scope) }) catch @panic("PRINT");
 
     const msg_fmt = format ++ "\n";
-    w.print(msg_fmt, args) catch return;
-
-    const written = fbs.getWritten();
-    file.writeAll(written) catch {};
+    w.print(msg_fmt, args) catch {};
 }
